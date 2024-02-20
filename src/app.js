7329e25846d284db495d00ce437da31c62d518c2
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-02-20 10:27:52 -0500

    adding a get request with restaurant parameters

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
+app.get("/restaurants", async (req, res) => {
+  const allRestaurants = await Restaurant.findAll();
+  res.json(allRestaurants);
+});
 
-
-
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  res.json(restaurant);
+});
 
 module.exports = app;
