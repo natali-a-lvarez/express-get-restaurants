1bf4ac603ab799f06ecd8b597577b30cd49e17b7
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-02-20 10:47:46 -0500

    creating post, put and delete requests for restaurants

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/restaurants", async (req, res) => {
   const allRestaurants = await Restaurant.findAll();
   res.json(allRestaurants);
@@ -12,6 +15,26 @@ app.get("/restaurants", async (req, res) => {
 app.get("/restaurants/:id", async (req, res) => {
   const restaurant = await Restaurant.findByPk(req.params.id);
   res.json(restaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const newRestaurant = await Restaurant.create(req.body);
+  res.json(newRestaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const updatedRestaurant = await Restaurant.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updatedRestaurant);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const deletedRestaurant = await Restaurant.destroy({
+    where: { id: req.params.id },
+  });
+
+  res.json(deletedRestaurant);
 });
 
 module.exports = app;
